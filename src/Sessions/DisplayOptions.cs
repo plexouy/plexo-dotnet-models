d6e57a85fb9f270cb1c397b13c2c9d50de13b572
Santiago Navatta <santiagouy07@gmail.com>
2021-09-06 12:34:53 -0300

    Nullable display options

--- src/Sessions/DisplayOptions.cs
+++ src/Sessions/DisplayOptions.cs
@@ -5,11 +5,11 @@ namespace Plexo.Models
     [DataContract]
     public class DisplayOptions
     {
-        [DataMember] public bool Titles { get; set; } // Mostrar titulos
-        [DataMember] public bool Details { get; set; } // Mostrar detalle de compra
-        [DataMember] public bool Footer { get; set; } // Mostrar pie de pagina
-        [DataMember] public bool Logo { get; set; } // Mostrar logo
-        [DataMember] public bool Accessibility { get; set; } // Activar accesibilidad usando equalweb
+        [DataMember] public bool? Titles { get; set; } // Mostrar titulos
+        [DataMember] public bool? Details { get; set; } // Mostrar detalle de compra
+        [DataMember] public bool? Footer { get; set; } // Mostrar pie de pagina
+        [DataMember] public bool? Logo { get; set; } // Mostrar logo
+        [DataMember] public bool? Accessibility { get; set; } // Activar accesibilidad usando equalweb
     }
 
 }
