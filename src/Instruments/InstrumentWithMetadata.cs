e6468c7f57d9507da70daa4e78d8e835fe00ba2a
Francisco Vidal <fvidal@plexo.com.uy>
2024-08-23 14:22:29 -0300

    OptionalBIN

--- src/Instruments/InstrumentWithMetadata.cs
+++ src/Instruments/InstrumentWithMetadata.cs
@@ -1,4 +1,4 @@
-﻿using System.Runtime.Serialization;
+using System.Runtime.Serialization;
 using Plexo.Models;
 
 namespace Plexo.Models
@@ -9,5 +9,6 @@ namespace Plexo.Models
         [DataMember] public PaymentInstrument PaymentInstrument { get; set; }
 
         [DataMember] public string OptionalMetadata { get; set; }
+        [DataMember] public string OptionalBIN { get; set; }
     }
 }
