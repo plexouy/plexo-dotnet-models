0844b32142bfe11d8e084b9e7e398bed1374c126
Francisco Vidal <fvidal@plexo.com.uy>
2024-10-18 08:45:20 -0300

    PurchaseRequest ThreeDSReferenceId optional parameter

--- src/Transactions/PaymentRequest.cs
+++ src/Transactions/PaymentRequest.cs
@@ -28,5 +28,6 @@ namespace Plexo.Models
         [DataMember] public string OptionalExternalPaymentInfo { get; set; }
         [DataMember] public decimal? LoyaltyProgramAmount { get; set; }
         [DataMember] public bool? IsExpressCheckout { get; set; }
+        [DataMember] public string ThreeDSReferenceId { get; set; }
     }
 }
