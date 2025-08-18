77adb3519c887342323a1083fab446fc053519cb
Santiago <santiagouy07@gmail.com>
2025-08-18 12:42:44 -0300

    Added new InclusionType values

--- src/Common/InclusionType.cs
+++ src/Common/InclusionType.cs
@@ -5,8 +5,17 @@ namespace Plexo.Models
     [DataContract]
     public enum InclusionType
     {
-        [EnumMember] None = 0,
-        [EnumMember] Law17934 = 1,
-        [EnumMember] Law19210 = 6
+        [EnumMember]
+        None = 0,
+        [EnumMember]
+        Law17934 = 1,
+        [EnumMember]
+        Law18083 = 2,
+        [EnumMember]
+        Law18910 = 3,
+        [EnumMember]
+        Law18999 = 4,
+        [EnumMember]
+        Law19210 = 6
     }
 }
